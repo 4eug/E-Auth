c2778c55a36775fe28f901e8dff1b0b6f9f01a89
4eug <debraheug@gmail.com>
2021-05-03 21:09:28 +0100

    login Screen Done!

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,6 +10,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // ignore: unused_field
   String _email, _password;
   final auth = FirebaseAuth.instance;
 
@@ -31,108 +32,110 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
               ),
             ),
-            SizedBox(height: 50),
-            _textField1(context),
+            SizedBox(height: 80),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.person),
+                  isCollapsed: false,
+                  focusedBorder: new OutlineInputBorder(
+                    borderSide:
+                        new BorderSide(color: Color(0xFF5ABD8C), width: 2),
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(20.0),
+                    ),
+                  ),
+                  enabledBorder: new OutlineInputBorder(
+                    borderSide:
+                        new BorderSide(color: Color(0x80EFEFEF), width: 2),
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(20.0),
+                    ),
+                  ),
+                  filled: true,
+                  hintStyle: new TextStyle(color: Color(0x75000000)),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _email = value.trim();
+                  });
+                },
+              ),
+            ),
             SizedBox(height: 20),
-            _textField2(context),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.lock),
+                  isCollapsed: false,
+                  focusedBorder: new OutlineInputBorder(
+                    borderSide:
+                        new BorderSide(color: Color(0xFF5ABD8C), width: 2),
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(20.0),
+                    ),
+                  ),
+                  enabledBorder: new OutlineInputBorder(
+                    borderSide:
+                        new BorderSide(color: Color(0x80EFEFEF), width: 2),
+                    borderRadius: const BorderRadius.all(
+                      const Radius.circular(20.0),
+                    ),
+                  ),
+                  filled: true,
+                  hintStyle: new TextStyle(color: Color(0x75000000)),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _password = value.trim();
+                  });
+                },
+              ),
+            ),
             SizedBox(height: 20),
-            _loginButton,
-            SizedBox(height: 50),
-            _already()
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: MaterialButton(
+                  height: 56,
+                  minWidth: double.infinity,
+                  color: Color(0xFF5ABD8C),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(20.0)),
+                  onPressed: () {},
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
+                  ),
+                )),
+            SizedBox(height: 60),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "You dont\'t have an account? ",
+                  style: GoogleFonts.poppins(fontSize: 18),
+                ),
+                InkWell(
+                  child: Text(
+                    "Register",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        decoration: TextDecoration.underline),
+                  ),
+                  onTap: () {},
+                )
+              ],
+            )
           ],
         ),
       ),
     );
   }
-}
-
-Widget _textField1(context) => Padding(
-      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: new InputDecoration(
-          labelText: 'Email',
-          prefixIcon: Icon(Icons.person),
-          isCollapsed: false,
-          focusedBorder: new OutlineInputBorder(
-            borderSide: new BorderSide(color: Color(0xFF5ABD8C), width: 2),
-            borderRadius: const BorderRadius.all(
-              const Radius.circular(20.0),
-            ),
-          ),
-          enabledBorder: new OutlineInputBorder(
-            borderSide: new BorderSide(color: Color(0x80EFEFEF), width: 2),
-            borderRadius: const BorderRadius.all(
-              const Radius.circular(20.0),
-            ),
-          ),
-          filled: true,
-          hintStyle: new TextStyle(color: Color(0x75000000)),
-        ),
-        onChanged: (value) {},
-      ),
-    );
-
-Widget _textField2(context) => Padding(
-      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-      child: TextField(
-        keyboardType: TextInputType.emailAddress,
-        decoration: new InputDecoration(
-          labelText: 'Password',
-          prefixIcon: Icon(Icons.lock),
-          isCollapsed: false,
-          focusedBorder: new OutlineInputBorder(
-            borderSide: new BorderSide(color: Color(0xFF5ABD8C), width: 2),
-            borderRadius: const BorderRadius.all(
-              const Radius.circular(20.0),
-            ),
-          ),
-          enabledBorder: new OutlineInputBorder(
-            borderSide: new BorderSide(color: Color(0x80EFEFEF), width: 2),
-            borderRadius: const BorderRadius.all(
-              const Radius.circular(20.0),
-            ),
-          ),
-          filled: true,
-          hintStyle: new TextStyle(color: Color(0x75000000)),
-        ),
-        onChanged: (value) {},
-      ),
-    );
-
-Widget get _loginButton => Padding(
-    padding: const EdgeInsets.all(20),
-    child: MaterialButton(
-      height: 56,
-      minWidth: double.infinity,
-      color: Color(0xFF5ABD8C),
-      shape:
-          RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
-      onPressed: () {},
-      child: Text(
-        "Log In",
-        style: TextStyle(fontSize: 15, color: Colors.white),
-      ),
-    ));
-
-Widget _already() {
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-      Text(
-        "Already have an account? ",
-        style: GoogleFonts.poppins(fontSize: 18),
-      ),
-      InkWell(
-        child: Text(
-          "Sign In",
-          style: GoogleFonts.poppins(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              decoration: TextDecoration.underline),
-        ),
-        onTap: () {},
-      )
-    ],
-  );
 }
