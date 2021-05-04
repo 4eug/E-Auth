979c2213d4a59d22775f8e19a1d1a43bfcbc0844
4eug <debraheug@gmail.com>
2021-05-04 17:45:38 +0100

    Phone Authentication

--- lib/screens/phoneAuth/login.dart
+++ lib/screens/phoneAuth/login.dart
@@ -1,5 +1,6 @@
+import 'package:e_auth/screens/home.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 enum MobileVerificationState {
   SHOW_MOBILE_FORM_STATE,
@@ -7,8 +8,6 @@ enum MobileVerificationState {
 }
 
 class LoginPhoneScreen extends StatefulWidget {
-  LoginPhoneScreen({Key key}) : super(key: key);
-
   @override
   _LoginPhoneScreenState createState() => _LoginPhoneScreenState();
 }
@@ -26,16 +25,48 @@ class _LoginPhoneScreenState extends State<LoginPhoneScreen> {
 
   bool showLoading = false;
 
+  void signInWithPhoneAuthCredential(
+      PhoneAuthCredential phoneAuthCredential) async {
+    setState(() {
+      showLoading = true;
+    });
+
+    try {
+      final authCredential =
+          await _auth.signInWithCredential(phoneAuthCredential);
+
+      setState(() {
+        showLoading = false;
+      });
+
+      if (authCredential?.user != null) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HomeScreen()));
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        showLoading = false;
+      });
+
+      _scaffoldKey.currentState
+          // ignore: deprecated_member_use
+          .showSnackBar(SnackBar(content: Text(e.message)));
+    }
+  }
+
   getMobileFormWidget(context) {
     return Column(
       children: [
+        Spacer(),
         TextField(
           controller: phoneController,
           decoration: InputDecoration(
             hintText: "Phone Number",
           ),
         ),
-        SizedBox(height: 16),
+        SizedBox(
+          height: 16,
+        ),
         // ignore: deprecated_member_use
         FlatButton(
           onPressed: () async {
@@ -44,33 +75,32 @@ class _LoginPhoneScreenState extends State<LoginPhoneScreen> {
             });
 
             await _auth.verifyPhoneNumber(
-                phoneNumber: phoneController.text,
-                verificationCompleted: (phoneAuthCredential) async {
-                  setState(() {
-                    showLoading = false;
-                  });
-                },
-                verificationFailed: (verificationFailed) async {
-                  setState(() {
-                    showLoading = false;
-                  });
-                  // ignore: deprecated_member_use
-                  _scaffoldKey.currentState.showSnackBar(
-                      SnackBar(content: Text(verificationFailed.message)));
-                },
-                codeSent: (verficationId, resendingToken) async {
-                  setState(() {
-                    showLoading = false;
-                    currentState =
-                        MobileVerificationState.SHOW_MOBILE_FORM_STATE;
-                    this.verificationId = verficationId;
-                  });
-                },
-                codeAutoRetrievalTimeout: (verificationId) async {});
+              phoneNumber: phoneController.text,
+              verificationCompleted: (phoneAuthCredential) async {
+                setState(() {
+                  showLoading = false;
+                });
+                //signInWithPhoneAuthCredential(phoneAuthCredential);
+              },
+              verificationFailed: (verificationFailed) async {
+                setState(() {
+                  showLoading = false;
+                });
+                // ignore: deprecated_member_use
+                _scaffoldKey.currentState.showSnackBar(
+                    SnackBar(content: Text(verificationFailed.message)));
+              },
+              codeSent: (verificationId, resendingToken) async {
+                setState(() {
+                  showLoading = false;
+                  currentState = MobileVerificationState.SHOW_OTP_FORM_STATE;
+                  this.verificationId = verificationId;
+                });
+              },
+              codeAutoRetrievalTimeout: (verificationId) async {},
+            );
           },
-          child: Text(
-            "SEND",
-          ),
+          child: Text("SEND"),
           color: Colors.blue,
           textColor: Colors.white,
         ),
@@ -82,19 +112,27 @@ class _LoginPhoneScreenState extends State<LoginPhoneScreen> {
   getOtpFormWidget(context) {
     return Column(
       children: [
+        Spacer(),
         TextField(
           controller: otpController,
           decoration: InputDecoration(
             hintText: "Enter OTP",
           ),
         ),
-        SizedBox(height: 16),
+        SizedBox(
+          height: 16,
+        ),
         // ignore: deprecated_member_use
         FlatButton(
-          onPressed: () {},
-          child: Text(
-            "VERFIY",
-          ),
+          onPressed: () async {
+            PhoneAuthCredential phoneAuthCredential =
+                PhoneAuthProvider.credential(
+                    verificationId: verificationId,
+                    smsCode: otpController.text);
+
+            signInWithPhoneAuthCredential(phoneAuthCredential);
+          },
+          child: Text("VERIFY"),
           color: Colors.blue,
           textColor: Colors.white,
         ),
@@ -108,17 +146,16 @@ class _LoginPhoneScreenState extends State<LoginPhoneScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
-      body: Container(
-        child: showLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : currentState == MobileVerificationState.SHOW_MOBILE_FORM_STATE
-                ? getMobileFormWidget(context)
-                : getOtpFormWidget(context),
-        padding: const EdgeInsets.all(16),
-      ),
-    );
+        key: _scaffoldKey,
+        body: Container(
+          child: showLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : currentState == MobileVerificationState.SHOW_MOBILE_FORM_STATE
+                  ? getMobileFormWidget(context)
+                  : getOtpFormWidget(context),
+          padding: const EdgeInsets.all(16),
+        ));
   }
 }
