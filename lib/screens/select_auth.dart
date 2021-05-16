813e9617daf04e71e82466bd700072d080950e0f
4eug <debraheug@gmail.com>
2021-05-16 22:25:51 +0100

    ..

--- lib/screens/select_auth.dart
+++ lib/screens/select_auth.dart
@@ -1,7 +1,6 @@
 import 'package:e_auth/screens/emailAuth/login.dart';
 import 'package:e_auth/screens/emailAuth/register.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class SelectAuthType extends StatefulWidget {
   SelectAuthType({Key key}) : super(key: key);
@@ -15,46 +14,76 @@ class _SelectAuthTypeState extends State<SelectAuthType> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(20, 100, 0, 0),
-              child: Center(
-                child: Text(
-                  "E Auth",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 100, 0, 0),
+                child: Center(
+                  child: Text(
+                    "E Auth",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-                padding: EdgeInsets.all(10.0),
-                child: SignInButton(
-                  Buttons.Email,
-                  text: "Register With Email",
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => RegisterScreen()),
-                    );
-                  },
-                )),
-            Padding(
-                padding: EdgeInsets.all(10.0),
-                child: GestureDetector(
-                    child: Text("Log In Using Email",
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            color: Colors.blue)),
-                    onTap: () {
+              SizedBox(height: 80),
+              Container(
+                width: double.infinity,
+                // ignore: deprecated_member_use
+                child: RaisedButton(
+                  onPressed: () {},
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Text(
+                          "Register With Your Email",
+                          style: TextStyle(
+                              fontSize: 23,
+                              color: Colors.white,
+                              letterSpacing: 0.168,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      )
+                    ],
+                  ),
+                  color: Color(0xFF5ABD8C),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: MaterialButton(
+                    height: 56,
+                    minWidth: double.infinity,
+                    color: Color(0xFF5ABD8C),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(20.0)),
+                    onPressed: () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => LoginScreen()),
                       );
-                    }))
-          ],
+                    },
+                    child: Text(
+                      "Log In Using Email",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
