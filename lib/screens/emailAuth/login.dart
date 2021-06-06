1d0bd276dd1526c6f62bcd377b4068e568de7844
4eug <debraheug@gmail.com>
2021-06-06 22:22:03 +0100

    Fixed RenderFlex

--- lib/screens/emailAuth/login.dart
+++ lib/screens/emailAuth/login.dart
@@ -52,153 +52,157 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   emailpasswrdWidget(context) {
-    bool isLoading = false;
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20, 100, 0, 0),
-          child: Center(
-            child: Text(
-              "Log In",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 100, 0, 0),
+            child: Center(
+              child: Text(
+                "Log In",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 80),
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              prefixIcon: Icon(Icons.email),
-              isCollapsed: false,
-              focusedBorder: new OutlineInputBorder(
-                borderSide: new BorderSide(color: Color(0xFF5ABD8C), width: 2),
-                borderRadius: const BorderRadius.all(
-                  const Radius.circular(20.0),
+          SizedBox(height: 80),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(Icons.email),
+                isCollapsed: false,
+                focusedBorder: new OutlineInputBorder(
+                  borderSide:
+                      new BorderSide(color: Color(0xFF5ABD8C), width: 2),
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(20.0),
+                  ),
                 ),
-              ),
-              enabledBorder: new OutlineInputBorder(
-                borderSide: new BorderSide(color: Color(0x80EFEFEF), width: 2),
-                borderRadius: const BorderRadius.all(
-                  const Radius.circular(20.0),
+                enabledBorder: new OutlineInputBorder(
+                  borderSide:
+                      new BorderSide(color: Color(0x80EFEFEF), width: 2),
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(20.0),
+                  ),
                 ),
+                filled: true,
+                hintStyle: new TextStyle(color: Color(0x75000000)),
               ),
-              filled: true,
-              hintStyle: new TextStyle(color: Color(0x75000000)),
+              onChanged: (value) {
+                setState(() {
+                  emailController.text = value.trim();
+                });
+              },
             ),
-            onChanged: (value) {
-              setState(() {
-                emailController.text = value.trim();
-              });
-            },
           ),
-        ),
-        SizedBox(height: 5),
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              prefixIcon: Icon(Icons.lock),
-              isCollapsed: false,
-              focusedBorder: new OutlineInputBorder(
-                borderSide: new BorderSide(color: Color(0xFF5ABD8C), width: 2),
-                borderRadius: const BorderRadius.all(
-                  const Radius.circular(20.0),
+          SizedBox(height: 5),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                isCollapsed: false,
+                focusedBorder: new OutlineInputBorder(
+                  borderSide:
+                      new BorderSide(color: Color(0xFF5ABD8C), width: 2),
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(20.0),
+                  ),
                 ),
-              ),
-              enabledBorder: new OutlineInputBorder(
-                borderSide: new BorderSide(color: Color(0x80EFEFEF), width: 2),
-                borderRadius: const BorderRadius.all(
-                  const Radius.circular(20.0),
+                enabledBorder: new OutlineInputBorder(
+                  borderSide:
+                      new BorderSide(color: Color(0x80EFEFEF), width: 2),
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(20.0),
+                  ),
                 ),
+                filled: true,
+                hintStyle: new TextStyle(color: Color(0x75000000)),
               ),
-              filled: true,
-              hintStyle: new TextStyle(color: Color(0x75000000)),
+              onChanged: (value) {
+                setState(() {
+                  passwordController.text = value;
+                });
+              },
             ),
-            onChanged: (value) {
-              setState(() {
-                passwordController.text = value;
-              });
-            },
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Text(
+                    "Forgot Password? ",
+                    style: GoogleFonts.roboto(
+                        fontSize: 18,
+                        color: Color(0xFF5ABD8C),
+                        fontWeight: FontWeight.w500),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ResetScreen()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(20),
+              child: isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : MaterialButton(
+                      height: 56,
+                      minWidth: double.infinity,
+                      color: Color(0xFF5ABD8C),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(20.0)),
+                      onPressed: () {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        _signin(emailController.text, passwordController.text);
+                      },
+                      child: Text(
+                        "Log In",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    )),
+          SizedBox(height: 60),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(
+                "You dont\'t have an account? ",
+                style: GoogleFonts.poppins(fontSize: 18),
+              ),
               InkWell(
                 child: Text(
-                  "Forgot Password? ",
-                  style: GoogleFonts.roboto(
+                  "Register",
+                  style: GoogleFonts.poppins(
                       fontSize: 18,
-                      color: Color(0xFF5ABD8C),
-                      fontWeight: FontWeight.w500),
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline),
                 ),
                 onTap: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => ResetScreen()),
+                    MaterialPageRoute(builder: (context) => RegisterScreen()),
                   );
                 },
-              ),
+              )
             ],
           ),
-        ),
-        Padding(
-            padding: const EdgeInsets.all(20),
-            child: isLoading
-                ? Center(child: CircularProgressIndicator())
-                : MaterialButton(
-                    height: 56,
-                    minWidth: double.infinity,
-                    color: Color(0xFF5ABD8C),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(20.0)),
-                    onPressed: () {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      _signin(emailController.text, passwordController.text);
-                    },
-                    child: Text(
-                      "Log In",
-                      style: TextStyle(fontSize: 15, color: Colors.white),
-                    ),
-                  )),
-        SizedBox(height: 60),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "You dont\'t have an account? ",
-              style: GoogleFonts.poppins(fontSize: 18),
-            ),
-            InkWell(
-              child: Text(
-                "Register",
-                style: GoogleFonts.poppins(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    decoration: TextDecoration.underline),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => RegisterScreen()),
-                );
-              },
-            )
-          ],
-        ),
-      ],
+        ],
+      ),
     );
   }
 
