e729cf76d82f3f073c86faea130069504125a7a8
4eug <debraheug@gmail.com>
2021-06-30 17:13:57 +0100

    Google SignIn

--- lib/screens/select_auth.dart
+++ lib/screens/select_auth.dart
@@ -1,6 +1,9 @@
 import 'package:e_auth/screens/emailAuth/login.dart';
 import 'package:e_auth/screens/emailAuth/register.dart';
+import 'package:e_auth/screens/googleAuth/authentication.dart';
+import 'package:e_auth/screens/home.dart';
 import 'package:e_auth/screens/qrcodeAuth/createQr.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SelectAuthType extends StatefulWidget {
@@ -11,6 +14,9 @@ class SelectAuthType extends StatefulWidget {
 }
 
 class _SelectAuthTypeState extends State<SelectAuthType> {
+  // ignore: unused_field
+  bool _isSigningIn = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -142,6 +148,57 @@ class _SelectAuthTypeState extends State<SelectAuthType> {
                           padding: const EdgeInsets.all(6.0),
                           child: Text(
                             "Log In Using QRCode",
+                            style: TextStyle(
+                                fontSize: 23,
+                                color: Colors.white,
+                                letterSpacing: 0.168,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        )
+                      ],
+                    ),
+                    color: Color(0xFF5ABD8C),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  width: double.infinity,
+                  // ignore: deprecated_member_use
+                  child: RaisedButton(
+                    onPressed: () async {
+                      setState(() {
+                        _isSigningIn = true;
+                      });
+                      User user = await Authentication.signInWithGoogle(
+                          context: context);
+
+                      setState(() {
+                        _isSigningIn = false;
+                      });
+
+                      if (user != null) {
+                        Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(builder: (context) => Home()));
+                      }
+                    },
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(30.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.mail_outline,
+                          color: Colors.white,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text(
+                            "Log In With Google",
                             style: TextStyle(
                                 fontSize: 23,
                                 color: Colors.white,
